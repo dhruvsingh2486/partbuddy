15860b33c0f88cbffb0fe1cf4bc217e65e23a083
Dhruv Singh <145809785+dhruvsingh2486@users.noreply.github.com>
2024-09-27 06:27:45 +0530

    Update Footer.jsx

--- src/Componantes/footer/Footer.jsx
+++ src/Componantes/footer/Footer.jsx
@@ -3,26 +3,26 @@ import "./footer.css";
 
 function Footer() {
   return (
-    <div
-      className="container-fluid footerbluearea"
-    >
+    <div className="container-fluid footerbluearea">
       <div className="row">
         <div className="col-md-6">
           <div
-            className="1st_side"
-            style={{ textAlign: "justify", margin: "15% 0 15% 20%" }}
+            className="first_side"
+            // style={{ textAlign: "justify", margin: "15% 0 15% 20%" }}
           >
             <div className="head_line">download</div>
             <div className="head_line2">our mobile app</div>
-            <p className="text">And get the full boodmo experience on the go</p>
           </div>
+          <p className="text">And get the full boodmo experience on the go</p>
         </div>
         <div
-          className="col-md-6"
-          style={{ marginTop: "6%", paddingLeft: "-8%", marginLeft: "-10%" }}
+          className="col-md-6 app-buttons"
+          // style={{ marginTop: "6%", paddingLeft: "-8%", marginLeft: "-10%" }}
         >
           <div class="footer_buttons">
-            <a href="https://www.apple.com/app-store/" classname="app-store" >
+            <a href="https://www.apple.com/app-store/" classname="app-store"
+            // style={{border:"3px solid yellow"}}
+            >
               {" "}
               <div className="icom_mgn">
                 <i class="fa-brands fa-app-store footer_icon"></i>
@@ -32,10 +32,10 @@ function Footer() {
             </a>
           </div>
 
-          <div class="footer_buttons-2">
+          <div class="footer_buttons">
             <a href="https://play.google.com/store" class="app-store">
               {" "}
-              <div className="icom_mgn2">
+              <div className="icom_mgn">
                 <i class="fa-brands fa-google-play"></i>
               </div>
               {/* <p className="line"> available on the </p> */}
